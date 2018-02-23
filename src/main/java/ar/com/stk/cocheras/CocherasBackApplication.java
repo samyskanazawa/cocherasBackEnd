7b0597534f18a717d482ca038e803a7ff6c6222c
Samys Kanazawa <samys.kanazawa.so@softtekosde.com>
2018-02-23 01:49:40 -0300

    Firt Commit

---
+++ src/main/java/ar/com/stk/cocheras/CocherasBackApplication.java
@@ -0,0 +1,12 @@
+package ar.com.stk.cocheras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocherasBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocherasBackApplication.class, args);
+	}
+}
